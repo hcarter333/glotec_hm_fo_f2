3b9406bfcb1707976cc0b583564754845609f92d
Automated <actions@users.noreply.github.com>
2025-03-04 22:23:39 +0000

    Update czmls with new content

--- build/lib/plugins/sqrt_pg.py
+++ build/lib/plugins/sqrt_pg.py
@@ -4,3 +4,6 @@ import math
 @hookimpl
 def prepare_connection(conn):
     conn.create_function("sqrt", 1, math.sqrt)
+    conn.create_function("sin", 1, math.sin)
+    conn.create_function("atan", 1, math.atan)
+    conn.create_function("cos", 1, math.cos)
